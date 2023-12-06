eee417d1d627d0c907d76a35074e88d34de5650a
RazmikA-Solicy <razmik.achikyan@solicy.net>
2023-12-06 20:47:46 +0400

    obsacle codes

--- src/models/Obstacle.ts
+++ src/models/Obstacle.ts
@@ -1,1 +1,21 @@
-export class Obstacle {}
+export class Obstacle {
+  private x: number;
+  private y: number;
+  private width: number;
+  private height: number;
+  constructor(x: number, y: number, width: number, height: number) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  public getPosition() {
+    return {
+      x: this.x,
+      y: this.y,
+      width: this.width,
+      height: this.height,
+    };
+  }
+}
